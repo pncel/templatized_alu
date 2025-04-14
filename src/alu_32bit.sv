0a1b98d45d070875d81a27689f0f2780a910891e
AyushKulk <ayush.kulkarni04@gmail.com>
2025-04-14 16:02:13 -0700

    ALL ALU combos generated

--- src/alu_32bit.sv
+++
@@ -1,9 +0,0 @@
-// alu_template.sv.j2
-module alu_32bit (
-    input  logic [31:0] A,
-    input  logic [31:0] B,
-    input  logic [2:0] opcode,
-    output logic [31:0] result
-);
-    // Insert ALU logic here
-endmodule
---
+++ src/generated/alu_32bit_gt.sv
@@ -0,0 +1,31 @@
+// alu_32bit.sv
+module alu_32bit (
+    input  logic [31:0] A,
+    input  logic [31:0] B,
+    input  logic [2:0] opcode,
+    output logic [31:0] result
+);
+
+    logic [31:0] _a, _b;
+    logic [31:0] add_sub_result;
+    logic carry_in;
+
+    // Shared input path
+    _a = A;
+    _b = B;
+    carry_in = 1'b0;
+
+
+    logic cmp_result;
+    logic gt_result = (A > B);
+
+    // Output mux
+    always_comb begin
+        result = '0;
+        case (opcode)
+            3'b010: result = 32'(gt_result);
+            default: result = '0;
+        endcase
+    end
+
+endmodule
