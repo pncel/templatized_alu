398eb6bea0383863b731999f2da26870cb8885fd
AyushKulk <ayush.kulkarni04@gmail.com>
2025-04-13 23:10:23 -0700

    Setup

---
+++ src/alu_32bit.sv
@@ -0,0 +1,9 @@
+// alu_template.sv.j2
+module alu_32bit (
+    input  logic [31:0] A,
+    input  logic [31:0] B,
+    input  logic [2:0] opcode,
+    output logic [31:0] result
+);
+    // Insert ALU logic here
+endmodule
